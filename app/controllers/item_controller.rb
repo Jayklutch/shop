8da5e0949e511c8da970e59352e69467fd59def3
Jayklutch <klutchdacar@outlook.com>
2015-05-12 22:05:22 +1200

    change color of text, test scss routing

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,5 +1,19 @@
 class ItemController < ApplicationController
   def index
     @items=Item.all
-end
+  end
+
+  def add
+    @items=Item.all
+  end
+
+  def edit
+    @items=Item.all
+  end
+
+  def delete
+    @items=Item.all
+  end
+
+  
 end
