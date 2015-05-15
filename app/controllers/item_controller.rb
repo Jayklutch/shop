dc617a150470e4af821801015d0285884e0b68b7
Jayklutch <klutchdacar@outlook.com>
2015-05-15 12:15:59 +1200

    remove console helpers

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -9,20 +9,11 @@ def add
 end
 
 def create
-  #@new=Item.create!(params)
-  puts "YO OVER HERE _______________________________________________________________"
-  puts params[:new][:name]
-
   @new = Item.create!(:name => params[:new][:name], :price=>params[:new][:price])
  redirect_to :action => 'add'
 end
 
   def delete
-    puts "_______________DELETING THIS STUFF ____________________________________________"
-
-    puts params
-    puts "and this is our id thingy"
-    puts params[:format]
     @vanish=Item.find_by name: (params[:format])
   @vanish.destroy!
   redirect_to :action=>'add'
